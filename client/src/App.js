3560cf1c232767d25235743f3e81162e7d45f2b4
Talha Idrees <talhaidrees116@gmail.com>
2023-10-14 01:31:12 +0500

    dockerrun

--- client/src/App.js
+++ client/src/App.js
@@ -19,8 +19,8 @@ function App() {
           >
             Learn React
           </a>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/">Home12345</Link>
+          <Link to="/otherpage">MY Page</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
